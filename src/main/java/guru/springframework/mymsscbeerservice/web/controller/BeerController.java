4f3e3c8a6d70eea5a4d7ce6e54d64d90ab866b3a
Nicu Taban <nictbndeveloper@gmail.com>
2022-05-06 00:00:27 +0300

    Adds Error Handling via ControllerAdvice

--- src/main/java/guru/springframework/mymsscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/mymsscbeerservice/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
@@ -17,13 +18,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
         // TODO IMPLEMENT
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
         // TODO IMPLEMENT
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
