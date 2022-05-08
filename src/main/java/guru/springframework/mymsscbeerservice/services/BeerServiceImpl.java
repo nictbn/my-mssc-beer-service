025d337583fa31201aa37b0b021c8914f5505d2c
Nicu Taban <nictbndeveloper@gmail.com>
2022-05-09 01:03:03 +0300

    Add ehcache for list beer and get beer by id Closes #20

--- src/main/java/guru/springframework/mymsscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/mymsscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import guru.springframework.mymsscbeerservice.web.model.BeerDto;
 import guru.springframework.mymsscbeerservice.web.model.BeerPagedList;
 import guru.springframework.mymsscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerMapper beerMapper;
 
     @Override
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = " #showInventoryOnHand == false")
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
         if (showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
@@ -46,6 +48,7 @@ public class BeerServiceImpl implements BeerService {
     }
 
     @Override
+    @Cacheable(cacheNames = "beerListCache", condition = " #showInventoryOnHand == false")
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
