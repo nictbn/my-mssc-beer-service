d10b3ac0ea94beb61894e5237ae605d0fd8ab174
Nicu Taban <nictbndeveloper@gmail.com>
2022-05-28 22:09:40 +0300

    Updates Inventory Service To Use OpenFeign. Closes #44

--- src/main/java/guru/springframework/mymsscbeerservice/MyMsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/mymsscbeerservice/MyMsscBeerServiceApplication.java
@@ -3,7 +3,9 @@ package guru.springframework.mymsscbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MyMsscBeerServiceApplication {
 
